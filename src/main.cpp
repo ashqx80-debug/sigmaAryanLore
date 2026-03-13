51cad042da2483d856cba80d44b1bb51ee0d8048
Ashwath Prabhu <ashwath@email.com>
2026-03-13 16:08:39 -0500

    Adjust autonomous selector for improved functionality in robot behavior

--- src/main.cpp
+++ src/main.cpp
@@ -85,7 +85,7 @@ void autonomous(){
     6 - sig sawp | not made
     7 - skills | not made
     */
-    int autonSelector = 7;
+    int autonSelector = 3;
     // chassis.setPose(estX, estY, estH * 180 / M_PI);
     Snacky.set_value(true);
     switch (autonSelector){    
